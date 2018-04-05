ee9df54ae32f9ad366e0a4a8b920ed006a43a192
Rina Mito <satoshikibu@gmail.com>
2018-04-05 11:37:16 +0900

    第11回

--- myapp.sql
+++ myapp.sql
@@ -5,13 +5,17 @@ create table users (
   score float
 );
 
--- alter table users add column email varchar(255);
-alter table users add column email varchar(255) after name;
-alter table users drop column score;
-alter table users change name user_name varchar(80) default 'nobody';
+insert into users (name, score) values ('haku', 8.9);
+insert into users (name, score) values ('yamato', 5.8);
+insert into users (name, score) values ('gino', 6.6);
+insert into users (name, score) values ('shura', 7.7);
+insert into users (name, score) values ('nico', null);
+insert into users (name, score) values ('peter', 9.9);
 
-desc users;
-
-alter table users rename persons;
-show tables;
-drop table if exists persons;
+-- select * from users;
+-- select id, name from users;
+-- select * from users where score >= 6.0 ;
+-- select * from users where score >= 3.0 and score <= 6.0 ;
+-- select * from users where score between 3.0 and 6.0 ;
+select * from users where name = 'haku' or name = 'shura';
+select * from users where name in ('haku', 'shura');
