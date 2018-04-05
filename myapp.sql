b3fd044686c73ff0b4002410495713e7808b4780
Rina Mito <satoshikibu@gmail.com>
2018-04-05 12:14:03 +0900

    第14回

--- myapp.sql
+++ myapp.sql
@@ -12,8 +12,12 @@ insert into users (name, score) values ('Shura', 7.7);
 insert into users (name, score) values ('nico', null);
 insert into users (name, score) values ('peter', 9.9);
 
--- select * from users order by score;
--- select * from users where score is not null order by score desc;
--- select * from users limit 3;
--- select * from users limit 3 offset 3;
-select * from users order by score desc limit 3;
+-- update users set score = 5.9;
+-- update users set score = 5.9 where id = 1;
+-- update users set name = 'maharo', score = 2.2 where name = 'nico';
+
+-- delete from users;
+delete from users where id = 2;
+delete from users where score <= 7.0;
+
+select * from users;
