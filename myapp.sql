bc11bc80b6e57ff41c744469f97f6d5f6fef0cbf
Rina Mito <satoshikibu@gmail.com>
2018-04-05 11:05:23 +0900

    第9回

--- myapp.sql
+++ myapp.sql
@@ -1,19 +1,22 @@
 drop table if exists users;
 create table users (
-  id int unsigned,
-  name varchar(20),
-  score float
+  id int unsigned primary key auto_increment,
+  name varchar(20) unique,
+  -- score float not null
+  score float default 0.0
 );
+desc users;
+
+insert into users (name, score) values (s'haku', 8.9);
+insert into users (name, score) values (s'yamato', 5.8);
+insert into users (name, score) values (s'gino', 6.6);
 
 -- insert into users (id, name, score) values (1, 'haku', 8.9);
 -- insert into users (id, name, score) values (2, 'yamato', 5.8);
 -- insert into users (id, name, score) values (3, 'gino', 6.6);
 -- insert into users (id, name, score) values (4, 'nico', null);
+-- insert into users (id, name) values (5, 'shura');
+-- insert into users (id, name) values (6, 'shura');
 
-insert into users (id, name, score) values
-  (1, 'haku', 8.9),
-  (2, 'yamato', 5.8),
-  (3, 'gino', 6.6),
-  (4, 'nico', null);
 
 select * from users;
