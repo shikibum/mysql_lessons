ac242417551d8238fa05072510cc56caa6419df0
Rina Mito <satoshikibu@gmail.com>
2018-04-05 14:03:45 +0900

    第17回

--- myapp.sql
+++ myapp.sql
@@ -2,22 +2,17 @@ drop table if exists users;
 create table users (
   id int unsigned primary key auto_increment,
   name varchar(20),
-  score float
+  score float,
+  rank enum('gold', 'silver', 'bronze')
 );
 
-insert into users (name, score) values ('haku', 8.9);
-insert into users (name, score) values ('yamato', 5.8);
-insert into users (name, score) values ('gino', 6.6);
+insert into users (name, score, rank) values ('haku', 8.9, 'silver');
+insert into users (name, score, rank) values ('yamato', 5.8, 'bronze');
+insert into users (name, score, rank) values ('gino', 6.6, 'red');
 insert into users (name, score) values ('Shura', 7.7);
 insert into users (name, score) values ('nico', null);
 insert into users (name, score) values ('peter', 9.9);
 
--- select length('hello'); -- 5
--- select substr('hello', 2); -- ello
--- select substr('hello', 2, 3); -- ell 2文字目以降の3文字
--- select lower('Hello'); -- hello
--- select upper('Hello'); -- HELLO
--- select concat('hello', 'world'); -- helloworld
-
--- select length(name), name from users order by length(name);
-select length(name) as len, name from users order by len;
+-- select * from users;
+-- select * from users where rank = 'silver';
+select * from users where rank = 2;
