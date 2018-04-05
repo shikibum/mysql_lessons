5eeee5663b420b5aac068c47097ee32028e2bf79
Rina Mito <satoshikibu@gmail.com>
2018-04-05 14:09:26 +0900

    第18回

--- myapp.sql
+++ myapp.sql
@@ -3,16 +3,17 @@ create table users (
   id int unsigned primary key auto_increment,
   name varchar(20),
   score float,
-  rank enum('gold', 'silver', 'bronze')
+  coins set('gold', 'silver', 'bronze')
 );
 
-insert into users (name, score, rank) values ('haku', 8.9, 'silver');
-insert into users (name, score, rank) values ('yamato', 5.8, 'bronze');
-insert into users (name, score, rank) values ('gino', 6.6, 'red');
+insert into users (name, score, coins) values ('haku', 8.9, 'gold,silver');
+insert into users (name, score, coins) values ('yamato', 5.8, 'silver,bronze');
+insert into users (name, score, coins) values ('gino', 6.6, 'red');
 insert into users (name, score) values ('Shura', 7.7);
 insert into users (name, score) values ('nico', null);
 insert into users (name, score) values ('peter', 9.9);
 
 -- select * from users;
--- select * from users where rank = 'silver';
-select * from users where rank = 2;
+-- select * from users where coins = 'gold,silver';
+-- select * from users where coins like '%silver%';
+select * from users where coins = 3;
