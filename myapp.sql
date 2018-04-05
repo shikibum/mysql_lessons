7d2abb041baf99d2f865202c1f109de0588bbdaf
Rina Mito <satoshikibu@gmail.com>
2018-04-05 14:27:25 +0900

    第20回

--- myapp.sql
+++ myapp.sql
@@ -12,28 +12,23 @@ insert into users (name, score) values ('Shura', 7.7);
 insert into users (name, score) values ('nico', null);
 insert into users (name, score) values ('peter', 4.0);
 
+-- create table users_with_team as
 -- select
+--   id,
 --   name,
 --   score,
---   if (score > 5.0, 'OK', 'NG') as result
+--   case
+--   when score > 8.0 then 'Team-A'
+--   when score > 6.0 then 'Team-B'
+--   else 'Team-C'
+--   end as team
 -- from users;
+--
+-- select * from users_with_team;
 
--- select
---   name,
---   score,
---   case floor(score) % 2
---   when 0 then 'even'
---   when 1 then 'odd'
---   else null
---   end as type
--- from users;
+-- create table users_copy select * from users;
+-- select * from users_copy;
 
-select
-  name,
-  score,
-  case
-  when score > 8.0 then 'Team-A'
-  when score > 6.0 then 'Team-B'
-  else 'Team-C'
-  end as team
-from users;
+create table users_empty like users;
+desc users_empty;
+select * from users_empty;
