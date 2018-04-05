ca079312389dcda0774f720d7c48e8a2e6539554
Rina Mito <satoshikibu@gmail.com>
2018-04-05 14:19:28 +0900

    第19回

--- myapp.sql
+++ myapp.sql
@@ -2,18 +2,38 @@ drop table if exists users;
 create table users (
   id int unsigned primary key auto_increment,
   name varchar(20),
-  score float,
-  coins set('gold', 'silver', 'bronze')
+  score float
 );
 
-insert into users (name, score, coins) values ('haku', 8.9, 'gold,silver');
-insert into users (name, score, coins) values ('yamato', 5.8, 'silver,bronze');
-insert into users (name, score, coins) values ('gino', 6.6, 'red');
+insert into users (name, score) values ('haku', 8.9);
+insert into users (name, score) values ('yamato', 5.8);
+insert into users (name, score) values ('gino', 6.6);
 insert into users (name, score) values ('Shura', 7.7);
 insert into users (name, score) values ('nico', null);
-insert into users (name, score) values ('peter', 9.9);
+insert into users (name, score) values ('peter', 4.0);
 
--- select * from users;
--- select * from users where coins = 'gold,silver';
--- select * from users where coins like '%silver%';
-select * from users where coins = 3;
+-- select
+--   name,
+--   score,
+--   if (score > 5.0, 'OK', 'NG') as result
+-- from users;
+
+-- select
+--   name,
+--   score,
+--   case floor(score) % 2
+--   when 0 then 'even'
+--   when 1 then 'odd'
+--   else null
+--   end as type
+-- from users;
+
+select
+  name,
+  score,
+  case
+  when score > 8.0 then 'Team-A'
+  when score > 6.0 then 'Team-B'
+  else 'Team-C'
+  end as team
+from users;
