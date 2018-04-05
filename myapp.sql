2f3d8b6a6a0c8e8f56e9a4b291e076c8231f58eb
Rina Mito <satoshikibu@gmail.com>
2018-04-05 10:57:58 +0900

    第8回

--- myapp.sql
+++ myapp.sql
@@ -1,29 +1,19 @@
-/*
-number:
-- int
-- float
-- double
-- int unsigned
-
-string:
-- char(4)
-- varchar(255)
-- text
-
-date/time:
-- date
-- time
-- datetime '2017-07-22 17:22:33'
-
-true/false:
-- boolean -> tinyint(1)
-  true -> 1
-  false -> 0
-*/
-
 drop table if exists users;
 create table users (
   id int unsigned,
   name varchar(20),
   score float
 );
+
+-- insert into users (id, name, score) values (1, 'haku', 8.9);
+-- insert into users (id, name, score) values (2, 'yamato', 5.8);
+-- insert into users (id, name, score) values (3, 'gino', 6.6);
+-- insert into users (id, name, score) values (4, 'nico', null);
+
+insert into users (id, name, score) values
+  (1, 'haku', 8.9),
+  (2, 'yamato', 5.8),
+  (3, 'gino', 6.6),
+  (4, 'nico', null);
+
+select * from users;
