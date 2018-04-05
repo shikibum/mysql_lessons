6b95c5e16a5a9a8525dc8dbd9fc70f3162b3fcc5
Rina Mito <satoshikibu@gmail.com>
2018-04-05 12:01:33 +0900

    第12回

--- myapp.sql
+++ myapp.sql
@@ -8,14 +8,12 @@ create table users (
 insert into users (name, score) values ('haku', 8.9);
 insert into users (name, score) values ('yamato', 5.8);
 insert into users (name, score) values ('gino', 6.6);
-insert into users (name, score) values ('shura', 7.7);
+insert into users (name, score) values ('Shura', 7.7);
 insert into users (name, score) values ('nico', null);
 insert into users (name, score) values ('peter', 9.9);
 
--- select * from users;
--- select id, name from users;
--- select * from users where score >= 6.0 ;
--- select * from users where score >= 3.0 and score <= 6.0 ;
--- select * from users where score between 3.0 and 6.0 ;
-select * from users where name = 'haku' or name = 'shura';
-select * from users where name in ('haku', 'shura');
+-- select * from users where name like 'h%';
+-- select * from users where name like '%h%';
+-- select * from users where name like binary 'S%';
+select * from users where name like binary '____';
+select * from users where name like binary '_i%';
