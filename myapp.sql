271ee8cce16b6a4bf7ecf43509577dc7c51b7fdc
Rina Mito <satoshikibu@gmail.com>
2018-04-06 16:06:07 +0900

    第30回

--- myapp.sql
+++ myapp.sql
@@ -8,9 +8,11 @@ create table posts (
 drop table if exists comments;
 create table comments (
   id int unsigned primary key auto_increment,
-  post_id int not null,
+  post_id int unsigned not null,
   body text
 );
+
+alter table comments add constraint fk_comments foreign key (post_id) references posts (id);
 
 insert into posts (title, body) values ('title 1', 'body 1');
 insert into posts (title, body) values ('title 2', 'body 2');
@@ -21,10 +23,5 @@ insert into comments (post_id, body) values (1, 'second comment for post 1');
 insert into comments (post_id, body) values (3, 'first comment for post 3');
 insert into comments (post_id, body) values (4, 'first comment for post 4');
 
--- inner join
--- outer join
-
--- select * from posts left outer join comments on posts.id = comments.post_id;
--- select * from posts right outer join comments on posts.id = comments.post_id;
-select * from posts left join comments on posts.id = comments.post_id;
-select * from posts right join comments on posts.id = comments.post_id;
+select * from posts;
+select * from comments;
