f87a6bd4a077033e5f9da8221917dd381b12f52d
Rina Mito <satoshikibu@gmail.com>
2018-04-06 16:20:00 +0900

    第31回

--- myapp.sql
+++ myapp.sql
@@ -21,7 +21,12 @@ insert into posts (title, body) values ('title 3', 'body 3');
 insert into comments (post_id, body) values (1, 'first comment for post 1');
 insert into comments (post_id, body) values (1, 'second comment for post 1');
 insert into comments (post_id, body) values (3, 'first comment for post 3');
-insert into comments (post_id, body) values (4, 'first comment for post 4');
+
+delete from posts where id = 2;
+insert into posts (title, body) values ('new title', 'new body');
+  -- insert into comments (post_id, body) values (4, 'first comment for new post');
+  insert into comments (post_id, body) values (last_insert_id(), 'first comment for new post');
+
 
 select * from posts;
 select * from comments;
