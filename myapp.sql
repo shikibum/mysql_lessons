93405ec012cd99f0717d088b212820d6a9cd128a
Rina Mito <satoshikibu@gmail.com>
2018-04-06 14:40:12 +0900

    第27回

--- myapp.sql
+++ myapp.sql
@@ -1,21 +1,25 @@
-drop table if exists users;
-create table users (
+drop table if exists posts;
+create table posts (
   id int unsigned primary key auto_increment,
-  name varchar(20),
-  score float
+  title varchar(255),
+  body text
 );
 
-insert into users (name, score) values ('haku', 8.9);
-insert into users (name, score) values ('yamato', 5.8);
-insert into users (name, score) values ('gino', 6.6);
-insert into users (name, score) values ('Shura', 7.7);
-insert into users (name, score) values ('nico', null);
-insert into users (name, score) values ('peter', 4.0);
+drop table if exists comments;
+create table comments (
+  id int unsigned primary key auto_increment,
+  post_id int not null,
+  body text
+);
 
-alter table users add index index_score (score);
--- show index from users;
--- explain select * from users where score > 5.0;
--- explain select * from users where name = 'haku';
+insert into posts (title, body) values ('title 1', 'body 1');
+insert into posts (title, body) values ('title 2', 'body 2');
+insert into posts (title, body) values ('title 3', 'body 3');
 
-alter table users drop index index_score;
--- show index from users;
+insert into comments (post_id, body) values (1,'first comment for post 1');
+insert into comments (post_id, body) values (1,'second comment for post 1');
+insert into comments (post_id, body) values (3,'first comment for post 3');
+insert into comments (post_id, body) values (4,'first comment for post 4');
+
+select * from posts;
+select * from comments;
