ab1323522602767c587c8f03406c640ff9a15056
Rina Mito <satoshikibu@gmail.com>
2018-04-06 16:49:00 +0900

    第33回

--- myapp.sql
+++ myapp.sql
@@ -10,14 +10,20 @@ create table logs (
   msg varchar(255)
 );
 
-drop trigger if exists posts_insert_trigger;
-create trigger posts_insert_trigger after insert on posts for each row insert into logs (msg) values ('post added!');
-
+drop trigger if exists posts_update_trigger;
+delimiter //
+create trigger posts_update_trigger after update on posts for each row
+  begin
+    insert into logs (msg) values ('post updated!');
+    insert into logs (msg) values (concat(old.title, ' -> ', new.title));
+  end;
+//
+delimiter ;
 insert into posts (title, body) values ('title 1', 'body 1');
 insert into posts (title, body) values ('title 2', 'body 2');
 insert into posts (title, body) values ('title 3', 'body 3');
 
+update posts set title = 'title 2 updated' where id = 2;
+
 select * from posts;
 select * from logs;
-
-show triggers \G;
