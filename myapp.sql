90bb42242185d88b966cdabc946ab74f4a2a6aa0
Rina Mito <satoshikibu@gmail.com>
2018-04-06 16:35:32 +0900

    第32回

--- myapp.sql
+++ myapp.sql
@@ -4,29 +4,20 @@ create table posts (
   title varchar(255),
   body text
 );
-
-drop table if exists comments;
-create table comments (
+drop table if exists logs;
+create table logs (
   id int unsigned primary key auto_increment,
-  post_id int unsigned not null,
-  body text
+  msg varchar(255)
 );
 
-alter table comments add constraint fk_comments foreign key (post_id) references posts (id);
+drop trigger if exists posts_insert_trigger;
+create trigger posts_insert_trigger after insert on posts for each row insert into logs (msg) values ('post added!');
 
 insert into posts (title, body) values ('title 1', 'body 1');
 insert into posts (title, body) values ('title 2', 'body 2');
 insert into posts (title, body) values ('title 3', 'body 3');
 
-insert into comments (post_id, body) values (1, 'first comment for post 1');
-insert into comments (post_id, body) values (1, 'second comment for post 1');
-insert into comments (post_id, body) values (3, 'first comment for post 3');
-
-delete from posts where id = 2;
-insert into posts (title, body) values ('new title', 'new body');
-  -- insert into comments (post_id, body) values (4, 'first comment for new post');
-  insert into comments (post_id, body) values (last_insert_id(), 'first comment for new post');
-
-
 select * from posts;
-select * from comments;
+select * from logs;
+
+show triggers \G;
